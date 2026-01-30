4d5503d22f8d01cad01e683c8a2c039e3a0791be
Coelacanthiform <coelacanthiform@proton.me>
2026-01-30 16:22:47 +0100

    Update local_ollama.lua

--- bin/lua/infra/AI/local_ollama.lua
+++ bin/lua/infra/AI/local_ollama.lua
@@ -82,7 +82,7 @@ function ollama.pick_speaker(msgs, callback)
 end
 
 function ollama.summarize_story(msgs, callback)
-	return send(msgs, callback, { model = MODEL.fast, temperature = 0.2, max_tokens = 2500 })
+	return send(msgs, callback, { model = MODEL.fast, temperature = 0.2, max_tokens = 3500 })
 end
 
 return ollama
